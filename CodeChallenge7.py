782714a6eb8568530db50df9c15fe66ab50c85db
Jilwin S. Palmaria <entityunknown431@gmail.com>
2025-10-27 12:05:11 +0800

    Add files via upload

---
+++ CodeChallenge7.py
@@ -0,0 +1,12 @@
+print("ODD NUMBER ACCUMULATOR")
+print("Enter 10 numbers. We'll sum only the odd ones!\n")
+
+odd_sum = 0
+
+for i in range(1, 11):
+    number = int(input(f"Enter number {i}: "))
+    
+    if number % 2 != 0:
+        odd_sum += number
+
+print(f"\nSum of all odd numbers: {odd_sum}")
